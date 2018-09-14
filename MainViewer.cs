a1654d84e710eea872889a78bb793f68aa82dcae
R Caloca <rcaloca@gmail.com>
2018-09-14 15:45:46 -0400

    Empty form

---
+++ MainViewer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VkDumpViewer
+{
+    public partial class MainViewer : Form
+    {
+        public MainViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var OFD = new OpenFileDialog();
+            var Result = OFD.ShowDialog();
+            if (Result == DialogResult.OK)
+            {
+                this.Text = OFD.FileName;
+            }
+        }
+    }
+}
